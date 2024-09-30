9d6b3c9bc59f4d9be69aebb4d9a9cd170fe6cbc6
MissedHeart <152282933+MissedHeart@users.noreply.github.com>
2024-10-01 02:57:56 +0800

    v0.7.8

--- game/03-JavaScript/funcSims.js
+++ game/03-JavaScript/funcSims.js
@@ -1,5 +1,167 @@
+function SchoolOrderVariableRefresh() {
+	if (document.getElementById('SchoolOrder_examine_0')!= null && document.getElementById('SchoolOrder_examine_0').checked == false)
+	{
+		V.SchoolOrder_examine_0 = false;
+	}
+	else
+	{
+		V.SchoolOrder_examine_0 = true;
+	}
+	if (document.getElementById('SchoolOrder_examine_1')!= null && document.getElementById('SchoolOrder_examine_1').checked == false)
+	{
+		V.SchoolOrder_examine_1 = false;
+	}
+	else
+	{
+		V.SchoolOrder_examine_1 = true;
+	}
+	if (document.getElementById('SchoolOrder_examine_2')!= null && document.getElementById('SchoolOrder_examine_2').checked == false)
+	{
+		V.SchoolOrder_examine_2 = false;
+	}
+	else
+	{
+		V.SchoolOrder_examine_2 = true;
+	}
+	if (document.getElementById('SchoolOrder_award_0')!= null && document.getElementById('SchoolOrder_award_0').checked == false)
+	{
+		V.SchoolOrder_award_0 = false;
+	}
+	else
+	{
+		V.SchoolOrder_award_0 = true;
+	}
+	if (document.getElementById('SchoolOrder_award_1')!= null && document.getElementById('SchoolOrder_award_1').checked == false)
+	{
+		V.SchoolOrder_award_1 = false;
+	}
+	else
+	{
+		V.SchoolOrder_award_1 = true;
+	}
+	if (document.getElementById('SchoolOrder_jobs_cook')!= null && document.getElementById('SchoolOrder_jobs_cook').checked == false)
+	{
+		V.SchoolOrder_jobs_cook = false;
+	}
+	else
+	{
+		V.SchoolOrder_jobs_cook = true;
+	}
+	if (document.getElementById('SchoolOrder_jobs_food')!= null && document.getElementById('SchoolOrder_jobs_food').checked == false)
+	{
+		V.SchoolOrder_jobs_food = false;
+	}
+	else
+	{
+		V.SchoolOrder_jobs_food = true;
+	}
+	if (document.getElementById('SchoolOrder_jobs_tool')!= null && document.getElementById('SchoolOrder_jobs_tool').checked == false)
+	{
+		V.SchoolOrder_jobs_tool = false;
+	}
+	else
+	{
+		V.SchoolOrder_jobs_tool = true;
+	}
+	if (document.getElementById('SchoolOrder_toys_0')!= null && document.getElementById('SchoolOrder_toys_0').checked == false)
+	{
+		V.SchoolOrder_toys_0 = false;
+	}
+	else
+	{
+		V.SchoolOrder_toys_0 = true;
+	}
+	if (document.getElementById('SchoolOrder_toys_1')!= null && document.getElementById('SchoolOrder_toys_1').checked == false)
+	{
+		V.SchoolOrder_toys_1 = false;
+	}
+	else
+	{
+		V.SchoolOrder_toys_1 = true;
+	}
+	if (document.getElementById('SchoolOrder_club_art')!= null && document.getElementById('SchoolOrder_club_art').checked == false)
+	{
+		V.SchoolOrder_club_art = false;
+	}
+	else
+	{
+		V.SchoolOrder_club_art = true;
+	}
+	if (document.getElementById('SchoolOrder_club_sex')!= null && document.getElementById('SchoolOrder_club_sex').checked == false)
+	{
+		V.SchoolOrder_club_sex = false;
+	}
+	else
+	{
+		V.SchoolOrder_club_sex = true;
+	}
+} window.SchoolOrderVariableRefresh = SchoolOrderVariableRefresh;
+
+function SchoolOrderJobsShow() {
+	if (document.getElementById('SchoolOrder_jobs_true')!= null) {
+		document.getElementById("SchoolOrder_jobs_true").classList.remove("noclickSims");
+	}
+} window.SchoolOrderJobsShow = SchoolOrderJobsShow;
+
+function SchoolOrderJobsHide() {
+	if (document.getElementById('SchoolOrder_jobs_cook')!= null && document.getElementById('SchoolOrder_jobs_cook').checked == true) {
+		document.getElementById('SchoolOrder_jobs_cook').checked = false;
+		V.SchoolOrder_jobs_cook = false;
+	}
+	if (document.getElementById('SchoolOrder_jobs_food')!= null && document.getElementById('SchoolOrder_jobs_food').checked == true) {
+		document.getElementById('SchoolOrder_jobs_food').checked = false;
+		V.SchoolOrder_jobs_food = false;
+	}
+	if (document.getElementById('SchoolOrder_jobs_tool')!= null && document.getElementById('SchoolOrder_jobs_tool').checked == true) {
+		document.getElementById('SchoolOrder_jobs_tool').checked = false;
+		V.SchoolOrder_jobs_tool = false;
+	}
+	if (document.getElementById('SchoolOrder_jobs_true') != null) {
+		document.getElementById("SchoolOrder_jobs_true").classList.add("noclickSims");
+	}
+} window.SchoolOrderJobsHide = SchoolOrderJobsHide;
+
+function SchoolOrderToysShow() {
+	if (document.getElementById('SchoolOrder_toys_sexual3') != null) {
+		document.getElementById("SchoolOrder_toys_sexual3").classList.remove("noclickSims");
+	}
+} window.SchoolOrderToysShow = SchoolOrderToysShow;
+
+function SchoolOrderToysHide() {
+	if (document.getElementById('SchoolOrder_toys_0')!= null && document.getElementById('SchoolOrder_toys_0').checked == true) {
+		document.getElementById('SchoolOrder_toys_0').checked = false;
+		V.SchoolOrder_toys_0 = false;
+	}
+	if (document.getElementById('SchoolOrder_toys_1')!= null && document.getElementById('SchoolOrder_toys_1').checked == true) {
+		document.getElementById('SchoolOrder_toys_1').checked = false;
+		V.SchoolOrder_toys_1 = false;
+	}
+	if (document.getElementById('SchoolOrder_toys_sexual3') != null) {
+		document.getElementById("SchoolOrder_toys_sexual3").classList.add("noclickSims");
+	}
+} window.SchoolOrderToysHide = SchoolOrderToysHide;
+
+function SchoolOrderClubShow() {
+	if (document.getElementById('SchoolOrder_club_true') != null) {
+		document.getElementById("SchoolOrder_club_true").classList.remove("noclickSims");
+	}
+} window.SchoolOrderClubShow = SchoolOrderClubShow;
+
+function SchoolOrderClubHide() {
+	if (document.getElementById('SchoolOrder_club_art')!= null && document.getElementById('SchoolOrder_club_art').checked == true) {
+		document.getElementById('SchoolOrder_club_art').checked = false;
+		V.SchoolOrder_club_art = false;
+	}
+	if (document.getElementById('SchoolOrder_club_sex')!= null && document.getElementById('SchoolOrder_club_sex').checked == true) {
+		document.getElementById('SchoolOrder_club_sex').checked = false;
+		V.SchoolOrder_club_sex = false;
+	}
+	if (document.getElementById('SchoolOrder_club_true') != null) {
+		document.getElementById("SchoolOrder_club_true").classList.add("noclickSims");
+	}
+} window.SchoolOrderClubHide = SchoolOrderClubHide;
+
 function robinRefuseTextFunc() {
-	SugarCube.Engine.play("Bedroom");
 	if(V.robin_refuse_text === true)
 	{
 		V.robin_refuse_text = false;
@@ -15,7 +177,8 @@ function robinRefuseTextFunc() {
 		document.getElementById("robin_refuse_text_span").innerText = "\n一张纸条贴在你的门上：“在休息！”你明白，这种东西无法挡住贝利。";
 		document.getElementById("robin_refuse_text_span").style.display = "inline";
 	}
-}window.robinRefuseTextFunc = robinRefuseTextFunc;
+	SugarCube.Engine.play("Bedroom");
+} window.robinRefuseTextFunc = robinRefuseTextFunc;
 
 function robinRefuseTextFunc2() {
 	if(V.robin_refuse_text === true)
@@ -36,32 +199,32 @@ function clearModSimsFunc() {
 	C.npc.Seath.init = 0;
 	V.NPCName.deleteWith(function (val) {return val.description === "Seath";});
 	V.NPCNameList.delete("Seath");
-}window.clearModSimsFunc = clearModSimsFunc;
+} window.clearModSimsFunc = clearModSimsFunc;
 
 function shutDownFunc() {
 	SugarCube.Engine.play("City Library -1");
-}window.shutDownFunc = shutDownFunc;
+} window.shutDownFunc = shutDownFunc;
 
 //function shutDownFuncBailey() {
 //	SugarCube.Engine.play("Bedroom Bailey");
-//}window.shutDownFuncBailey = shutDownFuncBailey;
+//} window.shutDownFuncBailey = shutDownFuncBailey;
 
 function compuNoLink() {
 	if(V.passage === "Computer Init Sims" || V.passage === "Computer Game Sims")
 		return false;
 	else
 		return true;
-}window.compuNoLink = compuNoLink;
+} window.compuNoLink = compuNoLink;
 
 function passTimePCSim(time) {
 	passTime(time);
 	document.getElementById("taskbar_day_ampm").textContent = ampmSims();
 	document.getElementById("origin_day_ampm").textContent = ampm();
 	document.getElementById("stat_time_Sims").textContent = ampm();
-}window.passTimePCSim = passTimePCSim;
+} window.passTimePCSim = passTimePCSim;
 
 function createFunWindow() {
-	passTimePCSim(10);
+	passTimePCSim(1);
 	//document.getElementById("sentence_1_0").style.display = "none";
 	//document.getElementById("sentence_2_0").style.display = "none";
 	//document.getElementById("sentence_3_0").style.display = "contents";
@@ -79,7 +242,7 @@ function createFunWindow() {
 			document.getElementById("windowSims_fun").style.display = "none";
 			document.getElementById("windowbarSims_fun").style.display = "none";
 			document.getElementById("sentence_1_0").textContent = "你回到了桌面上，思考着接下来要做的事情。";
-			//document.getElementById("sentence_2").textContent = "";
+			document.getElementById("sentence_2_0").textContent = "";
 		}
 	}
 	else
@@ -93,14 +256,14 @@ function createFunWindow() {
 	//document.getElementById("icon_window_esc").style.display = "inline-block";
 	//document.getElementById("").style.display = "inline-block";
 	//document.getElementsByClassName("windowiconSims").style.display = "inline-block";
-}window.createFunWindow = createFunWindow;
+} window.createFunWindow = createFunWindow;
 
 function shutDownFunWindow() {
 	document.getElementById("windowSims_fun").style.display = "none";
 	document.getElementById("windowbarSims_fun").style.display = "none";
 	document.getElementById("sentence_1_0").textContent = "你关闭了娱乐窗口，现在不是做这些的时候。";
 	document.getElementById("sentence_2_0").textContent = "";
-}window.shutDownFunWindow = shutDownFunWindow;
+} window.shutDownFunWindow = shutDownFunWindow;
 
 function changeFunWindow() {
 	if(document.getElementById("windowSims_fun").style.display == "block")
@@ -115,16 +278,63 @@ function changeFunWindow() {
 		document.getElementById("sentence_1_0").textContent = "你打开了娱乐界面，你可以在这里玩电子游戏，看视频，或是听音乐。一切都由你来选择。";
 		document.getElementById("sentence_2_0").textContent = "";
 	}
-}window.changeFunWindow = changeFunWindow;
+} window.changeFunWindow = changeFunWindow;
 
 function createGameWindow() {
 	document.getElementById("screen").style.display = "none";
 	document.getElementById("game_window").style.display = "block";
 	document.getElementById("sentence_1_0").textContent = "你跟游戏界面里孤零零的几个图标面面相觑，它们仿佛在无辜地看着你。或许你可以在网上购买一些新的游戏？";
 	document.getElementById("sentence_2_0").textContent = "";
-}window.createGameWindow = createGameWindow;
+} window.createGameWindow = createGameWindow;
 
 function createLiveWindow() {
+	passTimePCSim(1);
+	document.getElementById("sentence_1_0").textContent = "你打开了直播界面，许多知名主播都曾看过这个界面，就像你现在一样。";
+	document.getElementById("sentence_2_0").textContent = "";
+	if(document.getElementById("windowbarSims_live").style.display == "inline-block")
+	{
+		if(document.getElementById("windowSims_live").style.display == "none")
+		{
+			document.getElementById("windowSims_live").style.display = "block";
+		}
+		else
+		{
+			document.getElementById("windowSims_live").style.display = "none";
+			document.getElementById("windowbarSims_live").style.display = "none";
+			document.getElementById("sentence_1_0").textContent = "你回到了桌面上，思考着接下来要做的事情。";
+			document.getElementById("sentence_2_0").textContent = "";
+		}
+	}
+	else
+	{
+		document.getElementById("windowSims_live").style.display = "block";
+		document.getElementById("windowbarSims_live").style.display = "inline-block";
+	}
+} window.createLiveWindow = createLiveWindow;
+
+function shutDownLiveWindow() {
+	document.getElementById("windowSims_live").style.display = "none";
+	document.getElementById("windowbarSims_live").style.display = "none";
+	document.getElementById("sentence_1_0").textContent = "你关闭了直播窗口，现在不是做这些的时候。";
+	document.getElementById("sentence_2_0").textContent = "";
+} window.shutDownLiveWindow = shutDownLiveWindow;
+
+function changeLiveWindow() {
+	if(document.getElementById("windowSims_live").style.display == "block")
+	{
+		document.getElementById("windowSims_live").style.display = "none";
+		document.getElementById("sentence_1_0").textContent = "你回到了桌面上，思考着接下来要做的事情。";
+		document.getElementById("sentence_2_0").textContent = "";
+	}
+	else
+	{
+		document.getElementById("windowSims_live").style.display = "block";
+		document.getElementById("sentence_1_0").textContent = "你打开了直播界面，许多知名主播都曾看过这个界面，就像你现在一样。";
+		document.getElementById("sentence_2_0").textContent = "";
+	}
+} window.changeLiveWindow = changeLiveWindow;
+
+function createLiveWindow0() {
 	document.getElementById("screen").style.display = "none";
 	document.getElementById("live_window").style.display = "block";
 	document.getElementById("sentence_1_0").textContent = "你的观众们看上去";
@@ -146,7 +356,7 @@ function createLiveWindow() {
 	document.getElementById("sentence_2_7").textContent = "20.20%↓";
 	document.getElementById("sentence_2_7").style.color = "var(--red)";
 	document.getElementById("sentence_2_8").textContent = "。";
-}window.createLiveWindow = createLiveWindow;
+} window.createLiveWindow = createLiveWindow;
 
 function ampmSims(hour, minute) {
 	let ampm;
---
+++ game/03-JavaScript/timeHookSims.js
@@ -0,0 +1,66 @@
+const hookSims1 = [
+	(...args) => { V.Judgement_Examine_Finished = 0; },//风纪委员初始事件
+	(...args) => { V.City_Library_3rd_trespassing = 1; },//三楼每天最多撬锁一次
+	(...args) => { V.Seath_Noticed_Daily = 1; },//希斯第一阶段心防下降机制
+	(...args) => { V.kylar_abduction_today_Sims = 0; },//凯拉尔每日最多一狙
+	(...args) => { V.usedpraySims = 1; },//纳垢技能
+	(...args) => {
+		if (V.Gym_Card >= 2 && Time.weekDay !== 7) {
+			V.Gym_Card -= 1;
+		}
+	},//健身房每日扣费，周日除外
+	//https://github.com/Lyoko-Jeremie/DoLTimeWrapperAddonMod
+];
+hookSims1.forEach(hook => {
+	window.addonDoLTimeWrapperAddon.addFunctionHook({
+		key: 'dayPassed',
+		pos: 'after',
+		type: 'call',
+		hook: hook,
+	});
+});
+
+// const hookSims2 = [
+// 	(...args) => { V.willpower *= 1.2; },
+// ];
+// hookSims2.forEach(hook => {
+// 	window.addonDoLTimeWrapperAddon.addFunctionHook({
+// 		key: 'V.willpower *= 0.99;',
+// 		pos: 'before',
+// 		type: 'get',
+// 		hook: hook,
+// 		change: true,
+// 	});
+// });
+//有空得研究一下怎么截断返回值
+
+const hookSims3 = [
+	(...args) => {
+		if (Time.schoolTerm && V.scienceprojectwon == 1) {
+			V.science_exam = Math.clamp(V.science_exam + 7, -107, 200);
+		}
+	},//科学成绩增加，抵消衰减
+	(...args) => {
+		if (Time.schoolTerm && V.mathsprojectwon == 1) {
+			V.maths_exam = Math.clamp(V.maths_exam + 7, -107, 200);
+		}
+	},//数学成绩增加，抵消衰减
+	(...args) => {
+		if (Time.schoolTerm && V.englishPlayWell == 1) {
+			V.english_exam = Math.clamp(V.english_exam + 7, -107, 200);
+		}
+	},//语文成绩增加，抵消衰减
+	(...args) => {
+		if (Time.schoolTerm && V.museumAntiques.museumCount >= 25) {
+			V.history_exam = Math.clamp(V.history_exam + 7, -107, 200);
+		}
+	},//历史成绩增加，抵消衰减
+];
+hookSims3.forEach(hook => {
+	window.addonDoLTimeWrapperAddon.addFunctionHook({
+		key: 'weekPassed',
+		pos: 'before',
+		type: 'call',
+		hook: hook,
+	});
+});
